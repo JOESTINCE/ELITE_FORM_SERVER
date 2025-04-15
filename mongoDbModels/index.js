07697b1a47096f2080ba865a6e1c358df3f64547
Jestus A <jestusjoestince@gmail.com>
2025-04-15 14:27:57 +0530

    changes

--- mongoDbModels/index.js
+++ mongoDbModels/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`mongodb://${CONFIG.mongo_db_host}:${CONFIG.mongo_db_port}/${CONFIG.mongo_db_name}`);
+    // await mongoose.connect(`mongodb://${CONFIG.mongo_db_host}:${CONFIG.mongo_db_port}/${CONFIG.mongo_db_name}`);
+    await mongoose.connect(`mongodb+srv://atlas-sample-dataset-load-67fe1f157de07b4cf65b452e:<db_password>@cluster0.xal5osf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
     console.log('Connected to MongoDb database: ', CONFIG.mongo_db_name);
   } catch (err) {
     console.error('Error in connecting mongo db',err.message);
