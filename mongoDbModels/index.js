1daa8f7b66fd5eb2b3e8f603a4de5ca4f174a613
Jestus A <jestusjoestince@gmail.com>
2025-05-30 09:53:05 +0530

    db confif changes

--- mongoDbModels/index.js
+++ mongoDbModels/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     // await mongoose.connect(`mongodb://${CONFIG.mongo_db_host}:${CONFIG.mongo_db_port}/${CONFIG.mongo_db_name}`);
-    await mongoose.connect(`mongodb://joe_24:Jestus%4024@ac-vdwqmba-shard-00-00.xal5osf.mongodb.net:27017,ac-vdwqmba-shard-00-01.xal5osf.mongodb.net:27017,ac-vdwqmba-shard-00-02.xal5osf.mongodb.net:27017/your_db_name?replicaSet=atlas-1136zn-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority`);
+    await mongoose.connect(`mongodb+srv://jestusjoestince:11KbJMSKzV0TZtDF@cluster0.lug6lfa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
     console.log('Connected to MongoDb database: ', CONFIG.mongo_db_name);
   } catch (err) {
     console.error('Error in connecting mongo db',err.message);
